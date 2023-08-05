e7f07aef990f1c4b02b48bba599e56ea6100e863
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2023-08-05 12:04:04 +0100

    remove old types

--- app/types/types.ts
+++ app/types/types.ts
@@ -1,44 +1,7 @@
-export interface TaskType {
-  id: number
+export interface NodeDataType {
   title: string
   description: string
-  issues: IssueType[]
   done: boolean
-}
-
-export interface IssueType {
-  id: number
-  taskId: number
-  title: string
-  description: string
-  timeEstimate: string
-  done: boolean
-}
-
-export interface ProjectType {
-  id: number
-  name: string
-  tasks: TaskType[]
-  xarrowChangeCounter: number
-}
-
-type Action =
-  | { type: "EDIT_TASK_TITLE"; payload: TaskType }
-  | { type: "EDIT_TASK_DESCRIPTION"; payload: TaskType }
-  | { type: "EDIT_ISSUE_TITLE"; payload: IssueType }
-  | { type: "EDIT_ISSUE_DESCRIPTION"; payload: IssueType }
-  | { type: "EDIT_TASK_CHECKBOX"; payload: TaskType }
-  | { type: "EDIT_ISSUE_CHECKBOX"; payload: IssueType }
-  | { type: "DELETE_TASK"; payload: TaskType }
-  | { type: "DELETE_ISSUE"; payload: IssueType }
-  | { type: "NEW_PROJECT"; payload: ProjectType }
-  | { type: "CLEAR_PROJECT"; payload: ProjectType }
-
-export type DispatchType = (value: Action) => void
-
-export type ActionTypes = {
-  type: string
-  payload: TaskType | IssueType | ProjectType
 }
 
 export interface StreamContextProps {
